4a4ba8b62c7141b63522615a764830b64076a24e
Aaditya <aadityakadam2110@gmail.com>
2026-01-07 19:28:46 +0530

    Refactor OpenAI tool integration and enhance stock checking logic

--- core/main.py
+++ core/main.py
@@ -1,8 +1,8 @@
 import json
-from openai import OpenAI, Agent
+from openai import OpenAI
 from dotenv import load_dotenv
 
-load_dotenv(load_dotenv=True)
+load_dotenv()
 
 # datasource
 filepath = r"data/inventory.json"
@@ -34,7 +34,8 @@ def check_stock(file_path):
 
 # Define the tool for openai
 
-tools_schema = {
+tools = [
+    {
     "type": "function",
     "function": {
         "name": "check_stock",
@@ -51,3 +52,59 @@ tools_schema = {
         }
     }
 }
+]
+
+openai_client = OpenAI()
+
+messages = [
+    {
+        "role": "system",
+        "content": "You are a Ward Manager. You have a tool called check_stock. Always use it before answering."
+    },
+    {
+        "role": "user",
+        "content": "Do we have enough N95 masks?"
+    }
+]
+
+# first call: AI decides to use the tool
+response = openai_client.chat.completions.create(
+    model="gpt-4.1-nano",
+    messages=messages,
+    tools=tools
+)
+
+assistant_message = response.choices[0].message
+
+# if-else logic to handle tool usage
+
+if assistant_message.tool_calls:
+
+    #IMPORTANT: Append the assistant message before tool calls
+    messages.append(assistant_message)
+
+    # Process each tool call
+    for tool_call in assistant_message.tool_calls:
+        function_name = tool_call.function.name
+
+        if function_name != "check_stock":
+            print(f"Running tool: {function_name}...")
+
+        # step A: run your python function
+        result = check_stock(filepath)
+
+        # step B: append the tool response to the messages
+        messages.append({
+            "role": "tool",
+            "tool_call_id": tool_call.id, #receipt id
+            "content": result # actual data from the function
+        })
+
+    final_response = openai_client.chat.completions.create(
+        model="gpt-4.1-nano",
+        messages=messages
+    )
+    print(f"\nNHS Ward Manager: {final_response.choices[0].message.content}")
+else:
+    print(f"\nNHS Ward Manager: {assistant_message.content}")
+
